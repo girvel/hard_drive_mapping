da4db5981553e9c706ebd831b460196a9bce82e2
girvel <widauka@ya.ru>
2025-02-21 22:19:06 +0500

    Working mmap wrapper

--- main.c
+++ main.c
@@ -4,22 +4,52 @@
 #include <unistd.h>
 #include <assert.h>
 
-#define FILE_SIZE 16
-
 typedef struct {
-    int x;
-    int y;
-    int z;
-    char name[16];
-} Point;
+    int descriptor;
+    size_t size;
+    void *address;
+} MappedMemory;
+
+#define MAX_MAPPED_N 64
+MappedMemory map_stats[MAX_MAPPED_N];
+size_t map_stats_size = 0;
+
+void *map(const char *filename, size_t size) {
+    if (map_stats_size == MAX_MAPPED_N) return NULL;
+
+    int descriptor = open(filename, O_RDWR | O_CREAT);
+    if (descriptor == -1) return NULL;
+    if (ftruncate(descriptor, size) == -1) return NULL;
+
+    void* address = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, descriptor, 0);
+    if (address == NULL) return NULL;
+
+    map_stats[map_stats_size] = (MappedMemory) {
+        .descriptor = descriptor,
+        .size = size,
+        .address = address,
+    };
+    map_stats_size++;
+
+    return address;
+}
+
+void unmap(void *address) {
+    for (size_t i = 0; i < map_stats_size; i++) {
+        MappedMemory stat = map_stats[i];
+        if (stat.address == address) {
+            munmap(address, stat.size);
+            close(stat.descriptor);
+            map_stats[i] = map_stats[map_stats_size];
+            map_stats_size--;
+            return;
+        }
+    }
+}
 
 int main() {
-    int descriptor = open("storage", O_RDWR | O_CREAT);
-    assert(descriptor != -1 && "unable to open the file");
-    assert(ftruncate(descriptor, FILE_SIZE) != -1 && "unable to truncate file");
-
-    char *data = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, descriptor, 0);
-    assert(data != NULL && "mmap returned NULL");
+    const size_t FILE_SIZE = 16;
+    char *data = map("storage", FILE_SIZE);
 
     printf("Incrementing\n");
     for (size_t i = 0; i < FILE_SIZE; i++) {
@@ -31,8 +61,7 @@ int main() {
     }
     printf("\n");
 
-    munmap(data, FILE_SIZE);
-    close(descriptor);
+    unmap(data);
 
     return 0;
 }
